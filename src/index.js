303c4f1f613223abd9fcb1fa5fe427e96a37701b
Amit Gupta <you@example.com>
2019-06-07 20:55:24 +0530

    nc

--- src/index.js
+++ src/index.js
@@ -1,28 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
+  // constructor(props){
+  //     super(props);
 
-        this.state = {
-            lat:null,
-            errMessage:''
-        }
+  //     this.state = {
+  //         lat:null,
+  //         errMessage:''
+  //     }
+
+  // }
+
+  //babel will still convert below code as code above using constructor
+  state = { lat: null, errMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-        position=>this.setState({lat:position.coords.latitude}),
-        err=>this.setState({errMessage:err.message})
-    )
-    }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errMessage: err.message })
+    );
+  }
 
-    render(){
-            if(this.state.errMessage && !this.state.lat) return <div>Error: {this.state.errMessage}</div>
-            if(!this.state.errMessage && this.state.lat) return <div>Latitude: {this.state.lat}</div>
-            return <div>Loading!</div>
-    }
+  renderContent = () => {
+    if (this.state.errMessage && !this.state.lat)
+      return <div>Error: {this.state.errMessage}</div>;
+    if (!this.state.errMessage && this.state.lat)
+      return <SeasonDisplay lat={this.state.lat} />;
+    return <Spinner message="Please Accept Location Request" />;
+  };
+
+  render() {
+    return (
+      <div style={{ border: "10px solid red" }}>{this.renderContent()}</div>
+    );
+  }
 }
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
